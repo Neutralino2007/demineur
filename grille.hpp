1cc3fc0e8946fc3fab66ade4d7c028cb984373d2
Neutralino <91826738+Neutralino2007@users.noreply.github.com>
2025-12-10 20:41:21 +0100

    Update grille.hpp

--- grille.hpp
+++ grille.hpp
@@ -2,7 +2,7 @@
 #include "types.hpp"
 #include <vector>
 
-
+// variables globales pour gestions d'affichage et de victoire, cf grille.cpp
 extern int drapeaux_restants;
 extern int cases_decouvertes;
 
@@ -18,4 +18,5 @@ int cliquer_case(int i, int j, bool drap, mat & m);
 mat initialisation_grille();
 
 mat initialisation_grille_deduc(mat & m, int f);
+
 vector<int> deductions(mat & m, mat & mat_deduc);
